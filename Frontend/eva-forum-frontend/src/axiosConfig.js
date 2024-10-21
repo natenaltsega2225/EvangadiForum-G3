b34891cae521980112cc3dcecd9c2e33f2a11e3a
Your Name <you@example.com>
2024-10-21 03:44:29 +0300

    axios updated

--- Frontend/eva-forum-frontend/src/axiosConfig.js
+++ Frontend/eva-forum-frontend/src/axiosConfig.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
-const token = localStorage.getItem('token');
+// const token = localStorage.getItem('token');
 
 const axiosBase = axios.create({
-  // baseURL: 'http://localhost:5500/api', // using  local host 
-  baseURL:'https://evan-forum-deploy.onrender.com/api',  // using Render
-  headers: {
-    Authorization: token ? `Bearer ${token}` : '',
-  },
+  // baseURL: "http://localhost:5500/api", // using  local host
+  baseURL:'https://forumproject-1.onrender.com',  // using Render
+  // headers: {
+    // Authorization: token ? `Bearer ${token}` : "",
+  // },
+
 });
 
 
